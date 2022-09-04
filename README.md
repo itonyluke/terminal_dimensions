2ab31864ef0e079362b4e1a9c652cebb25406ebd
wanisha <wanisha@student.21-school.ru>
2022-09-04 22:45:38 +0400

    add_readme_exampleImage

---
+++ README.md
@@ -0,0 +1,1 @@
+This program outputs terminal dimensions, rows by columns and pixels by pixels
--- terminal_dimensions.c
+++ terminal_dimensions.c
@@ -1,3 +1,24 @@
+//Copyright 2022 Anton Pavliukevich.
+
+//terminal_dimensions is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//terminal_dimensions is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with terminal_dimensions.  If not, see <https://www.gnu.org/licenses/>.
+
+//
+// Created by pavliukevich.anton@gmail.com on 04.09.22
+//
+
+// built and tested on Darwin Kernel Version 21.6.0: Wed Aug 10 14:28:23 PDT 2022; root:xnu-8020.141.5~2/RELEASE_ARM64_T6000 arm64
+
 #include <sys/ioctl.h>
 #include <stdio.h>
 #include <string.h>
@@ -47,11 +68,11 @@ int main(void)
   if (get_terminal_window_size(&s) != 0)
 	  return (1);
 
-  printf("%i ", s.ws_col);
-  output_in_color("colomns by ");
-
   printf("%i", s.ws_row);
-  output_in_color(" rows\n");
+  output_in_color(" rows by ");
+
+  printf("%i ", s.ws_col);
+  output_in_color("colomns\n");
 
   printf("%i ", s.ws_xpixel);
   output_in_color("pixels by ");
